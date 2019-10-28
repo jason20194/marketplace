f8930f1faaaefc7e82df1dc905144468c76a132b
Jason Liu <jasonliu20194@gmail.com>
2019-10-28 14:33:03 +1100

    model Listing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_28_025631) do
+ActiveRecord::Schema.define(version: 2019_10_28_033107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2019_10_28_025631) do
     t.text "description"
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -36,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_10_28_025631) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "listings", "users"
 end
